687c55cf66b8eab8c22d08bc7f4580d431070b4b
Dewaley <adewaleademilua@gmail.com>
2022-02-14 03:48:26 +0100

    Input component

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Input = () => {
+  return (
+    <div>
+     <span>+</span>
+     <input type="text" name="" id="" placeholder='Enter Task'/>
+    </div>
+  )
+}
+
+export default Input
