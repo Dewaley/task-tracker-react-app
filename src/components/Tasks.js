5a9031ce438684a97097defc3620f9123ba23fc3
Dewaley <adewaleademilua@gmail.com>
2022-02-20 17:31:47 +0100

    TodoTemplate

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import { BsCircle, BsCheck } from 'react-icons/bs';
+import { FaTimes } from "react-icons/fa";
 
 const Tasks = ({todos}) => {
-  return todos.map((todo) => <BsCheck className='icon-tick' />);
+  return todos.map((todo) => (
+    <div className='todoTemplate'>
+      {/* <label>
+          {todo.isComplete ? (
+            <BsCheck className='icon-tick' />
+          ) : (
+            <BsCircle className='icon-circle' />
+          )}
+          <span className='list'>{todo.text}</span>
+        </label> */}
+      <label>
+        <BsCircle  className='checkCircle'/>
+        <span className='list'>Boss Man</span>
+      </label>
+      <div className='deleteTask'>
+        <FaTimes />
+      </div>
+    </div>
+  ));
 }
 
 export default Tasks
