210dbcf3b9b36fdae7617260a470a2c5296ff430
Dewaley <adewaleademilua@gmail.com>
2022-02-22 05:03:11 +0100

    Created Options Component

---
+++ src/components/Options.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Options = () => {
+  return (
+    <div className='option'>
+      <div>All</div>
+      <div>Active</div>
+      <div>Completed</div>
+    </div>
+  );
+}
+
+export default Options
