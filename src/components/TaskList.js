fa311cf25d05866702f252b989600c3e5a96889f
Dewaley <adewaleademilua@gmail.com>
2022-02-22 05:24:01 +0100

    Styled options and cleaner code

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import Todo from './Todo';
 
-const TaskList = ({ todos, handleCheck, deleteTask,clearCompleted }) => {
+const TaskList = ({ todos, handleCheck, deleteTask, clearCompleted }) => {
   return (
-    <div className='todoList'>
-      {todos.length > 0 && (
-        <div>
-          {todos.map((todo) => {
-            return (
-              <Todo
-                handleCheck={handleCheck}
-                id={todo.id}
-                isComplete={todo.isComplete}
-                text={todo.text}
-                deleteTask={deleteTask}
-              />
-            );
-          })}
-          <div className='infoAndActivity'>
-            <p>{todos.length} items left</p>
-            <p onClick={clearCompleted}>Clear Completed</p>
+    <div>
+      <div>
+        {todos.length > 0 && (
+          <div className='container'>
+            <div className='todoList'>
+              {todos.map((todo) => {
+                return (
+                  <Todo
+                    handleCheck={handleCheck}
+                    id={todo.id}
+                    isComplete={todo.isComplete}
+                    text={todo.text}
+                    deleteTask={deleteTask}
+                  />
+                );
+              })}
+              <div className='infoAndActivity'>
+                <p>{todos.length} items left</p>
+                <p onClick={clearCompleted} style={{cursor: "pointer"}}>Clear Completed</p>
+              </div>
+            </div>
+            <div className='options'>
+              <span>All</span>
+              <span>Active</span>
+              <span>Completed</span>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
